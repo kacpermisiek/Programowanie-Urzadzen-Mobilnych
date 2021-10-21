b9f6135feeb11fea609cf5f802147a641b7673a3
kacpermisiek <315560@uwr.edu.pl>
2021-10-21 18:34:46 +0200

    Zad2 Update
    Buttons added (without functionality)

---
+++ Lista 1/Zad2 - Calculator/Calculator2/app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Button button1, button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //textView = findViewById(R.id.textView);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+
+
+    }
+
+    public void onDecimalButton(View view) {
+        textView.append("0");
+    }
+}
