c6da23305b1d6e6528bee8959722b19428758e6c
kacpermisiek <315560@uwr.edu.pl>
2021-11-25 09:43:33 +0100

    Code Refactor

--- Lista 3/app/src/main/java/com/example/crimeapp/MainActivity.java
+++ Lista 3/app/src/main/java/com/example/crimeapp/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.crimeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.LinkedList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private LinkedList<String> wordlList = new LinkedList<>();
+    private RecyclerView recyclerView;
+    private WordListAdapter wordListAdapter;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for (int i = 0; i < 50; i ++)
+        {
+            wordlList.addLast("Word" + i);
+        }
+
+        recyclerView = findViewById(R.id.recyclerView);
+        wordListAdapter = new WordListAdapter(this, wordlList);
+        recyclerView.setAdapter(wordListAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
---
+++ Lista 3/app/src/main/java/com/example/crimeapp/WordListAdapter.java
@@ -0,0 +1,64 @@
+package com.example.crimeapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
+
+public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
+
+    private final LinkedList<String> wordList;
+    private LayoutInflater inflater;
+
+    public WordListAdapter(Context context, LinkedList<String> wordList){
+        inflater = LayoutInflater.from(context);
+        this.wordList = wordList;
+    }
+
+    class WordViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        public final TextView wordText;
+        final WordListAdapter adapter;
+
+        public WordViewHolder(@NonNull View itemView, WordListAdapter adapter){
+            super(itemView);
+            wordText = itemView.findViewById(R.id.word);
+            this.adapter = adapter;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getLayoutPosition();
+            String element = wordList.get(position);
+            wordList.set(position, "Clicked!" + element);
+            // adapter.notifyDataSetChanged();
+            adapter.notifyItemChanged(position);
+        }
+    }
+
+    @NonNull
+    @Override
+    public WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = inflater.inflate(R.layout.word_list_item, parent, false);
+        return new WordViewHolder(itemView, this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WordViewHolder holder, int position) {
+        String current = wordList.get(position);
+        holder.wordText.setText(current);
+    }
+
+    @Override
+    public int getItemCount() {
+        return wordList.size();
+    }
+
+}
